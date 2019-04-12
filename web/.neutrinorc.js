85d9c908d56a601f98c59df269eeca4bb934ba5d
Spencer Miller <smiller@instructure.com>
2019-04-11 19:17:28 -0600

    Initial web setup using neutrino

---
+++ web/.neutrinorc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  use: [
+    '@neutrinojs/airbnb',
+    [
+      '@neutrinojs/react',
+      {
+        html: {
+          title: 'Chorelock Holmes'
+        }
+      }
+    ],
+    '@neutrinojs/jest'
+  ]
+};
